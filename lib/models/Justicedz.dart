2ed756c1916344622fb90920dccbad5c1ed2dc8e
kacemws <berras.belkacem@gmail.com>
2020-08-13 20:48:01 +0100

    fixed some search issues

--- lib/models/Justicedz.dart
+++ lib/models/Justicedz.dart
@@ -63,7 +63,7 @@ class Justicedz with ChangeNotifier{
 
     if(keywords != ""){
       peoples.forEach((person){
-        if(("maitre " + person.nom+ " "+ person.prenom).contains(keywords)) aux.add(person);
+        if(("maitre " + person.nom.toLowerCase()+ " "+ person.prenom.toLowerCase()).contains(keywords.toLowerCase())) aux.add(person);
       });
       return aux;
     }
